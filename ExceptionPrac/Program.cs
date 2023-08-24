52cac60fcc7698d2277bd339067aa1c35ab050a8
Nagyeong Park <zzum0701@naver.com>
2023-08-24 14:39:55 +0900

    커스텀 예외 객체 생성 및 활용한 예외 처리

--- ExceptionPrac/Program.cs
+++ ExceptionPrac/Program.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace ExceptionPrac
 {
-    class Program
+    internal class Program
     {
+        // 입력 숫자는 0이상 999 이하여야 한다.
+        public class WrongNumberException : Exception
+        {
+        }
+
         static void Main(string[] args)
         {
             string[] array = { "가", "나" };
@@ -16,6 +21,12 @@ namespace ExceptionPrac
             try
             {
                 int input = int.Parse(Console.ReadLine());
+
+                if(input < 0 || input > 999)
+                {
+                    throw new WrongNumberException();
+                }
+
                 Console.WriteLine("입력한 위치의 값은 '"
                     + array[input] + "'입니다.");
             } 
@@ -26,6 +37,10 @@ namespace ExceptionPrac
             catch(FormatException ex)
             {
                 Console.WriteLine("숫자를 입력해주세요.");
+            }
+            catch (WrongNumberException ex)
+            {
+                Console.WriteLine("올바른 숫자를 입력해주세요.(0~999)");
             }
             catch(Exception ex)
             {
